ff4ede8052503ad6c1f58519b0da2b015f01f1bf
karla <karla@getharvest.com>
2021-03-23 13:42:30 -0700

    create Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,41 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+//Phrase class
+
+class Phrase {
+
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    const list = document.getElementById('phrase').firstElementChild;
+    const letters = [];
+
+    for (let i = 0; i < this.phrase.length; i++) {
+      letters.push(this.phrase[i]);
+    }
+
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i] === ' ') {
+        list.innerHTML += `<li class="space"> </li>`;
+      } else {
+        list.innerHTML += `<li class="hide letter ${letters[i]}">${letters[i]}</li>`;
+      }
+    }
+  }  
+
+  checkLetter(letter) {
+    if (this.phrase.includes(letter)) {
+      this.showMatchedLetter(letter);
+    }
+  }
+
+  showMatchedLetter(letter) {
+    const letterListItems = document.querySelector('ul').children;
+    for (let i=0; i < letterListItems.length; i++) {
+      if (letterListItems[i].textContent === letter) {
+        letterListItems[i].className = `show letter ${letter}`;
+      }
+    }
+  }
+
+}
