99e37a7bf88c858ab72007c4ba80be32434e672a
karla <karla@getharvest.com>
2021-03-25 13:00:39 -0700

    work with keyboard key presses

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let game = null;
 const playButton = document.getElementById('btn__reset');
 const keyboard = document.getElementById('qwerty');
+const keyButtons = document.getElementsByClassName('key');
 
 //Listen for clicks on the start/play game button
 playButton.addEventListener('click', () => {
@@ -14,5 +15,15 @@ playButton.addEventListener('click', () => {
 keyboard.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     game.handleInteractions(event.target, event.target.textContent);
+  }
+});
+
+//Listen for key presses on the player's actual keyboard
+document.addEventListener('keydown', (event) => {
+  for (let i = 0; i < keyButtons.length; i++) {
+    let letter = keyButtons[i].textContent;
+    if (event.key === letter) {
+      game.handleInteractions(keyButtons[i], letter);
+    }
   }
 });
